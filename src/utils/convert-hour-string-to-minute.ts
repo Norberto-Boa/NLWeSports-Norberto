88d57be25348d0d29f6c4dbb8b80f2d0fff1555b
Norberto Boa <nboa.26@gmail.com>
2022-11-07 22:52:40 +0200

    Remove duplicated directory

--- src/utils/convert-hour-string-to-minute.ts
+++
@@ -1,7 +0,0 @@
-export function convertHourToMinutes(hourString : string){
-  const [hours, minutes] = hourString.split(':').map(Number);
-
-  const minutesAmount = (hours * 60) + minutes;
-
-  return minutesAmount;
-}
